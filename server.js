cfbe273ca44ec1c97c386f388e6915fbb0f12cd7
PauloBittencourt <pbittencourtjr@gmail.com>
2021-11-16 22:18:43 -0300

    .

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const axios = require('axios')
+
+app.use(cors())
+
+
+app.get('/', async (req, res) => {
+
+    try {
+        // response é a respostas do axios MAS eu tiro o data de dentro do response
+  const {data} =  await axios('https://jsonplaceholder.typicode.com/users')
+
+    return res.json(data)
+    } catch (error) {
+        console.error(error)    
+    }
+
+
+})
+
+app.listen('4567')
